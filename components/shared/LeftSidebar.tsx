8342ba889a1e77538d115d3535a63795cad85427
bsegers <briansegs@gmail.com>
2024-05-09 16:21:41 -0400

    feat: Add NavContent

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -1,14 +1,39 @@
-import { SignedOut } from "@clerk/nextjs";
+"use client";
+
 import Link from "next/link";
+import Image from "next/image";
+import { sidebarLinks } from "@/constants";
+import { SignedOut } from "@clerk/nextjs";
 import { Button } from "@/components/ui/button";
 
 const NavContent = () => {
-  return <section>NavContent</section>;
+  return (
+    <section className="flex flex-1 flex-col gap-6">
+      {sidebarLinks.map(({ imgURL, route, label }) => {
+        return (
+          <Link
+            href={route}
+            key={route}
+            className="flex items-center justify-start gap-4 p-4"
+          >
+            <Image
+              src={imgURL}
+              alt={label}
+              height={20}
+              width={20}
+              className="invert-colors"
+            />
+            <p className="base-medium">{label}</p>
+          </Link>
+        );
+      })}
+    </section>
+  );
 };
 
 const LeftSidebar = () => {
   return (
-    <div className="light-border background-light900_dark200 sticky flex h-screen w-64 flex-col items-center justify-between gap-6 border-r p-6 pt-36">
+    <div className="light-border background-light900_dark200 custom-scrollbar sticky left-0 top-0 flex h-screen w-fit flex-col items-center justify-between gap-6 overflow-y-auto border-x p-6 pt-36 shadow-light-300 dark:shadow-none ">
       <NavContent />
 
       <SignedOut>
