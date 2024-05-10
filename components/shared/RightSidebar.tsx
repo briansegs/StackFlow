bf552011c32cc184ad315f4945eb1a16f3da6458
bsegers <briansegs@gmail.com>
2024-05-10 15:41:31 -0400

    feat: Add Questions

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -1,11 +1,54 @@
+import Link from "next/link";
+import Image from "next/image";
 import React from "react";
+
+const hotQuestions = [
+  {
+    id: 1,
+    title:
+      "Best practices for data fetching in a Next.js application with Server-Side Rendering (SSR)?",
+  },
+  {
+    id: 2,
+    title: "Is it only me or the font is bolder than necessary?",
+  },
+  {
+    id: 3,
+    title: "Redux Toolkit Not Updating State as Expected",
+  },
+  {
+    id: 4,
+    title: "Can I get the course for free?",
+  },
+  {
+    id: 5,
+    title: "Async/Await Function Not Handling Errors Properly",
+  },
+];
 
 const RightSidebar = () => {
   return (
     <section className="light-border background-light900_dark200 custom-scrollbar sticky right-0 top-0 flex h-screen w-fit flex-col  gap-6 overflow-y-auto border-l p-6 pt-36 shadow-light-300 dark:shadow-none max-xl:hidden lg:w-[350px]">
       <div>
         <h3 className="h3-bold text-dark200_light900">Top Questions</h3>
-        <div className="mt-7 flex w-full flex-col gap-[30px]"></div>
+        <div className="mt-7 flex w-full flex-col gap-[30px]">
+          {hotQuestions.map(({ id, title }) => (
+            <Link
+              href={`/questions/${id}`}
+              key={id}
+              className="flex cursor-pointer items-center justify-between gap-7"
+            >
+              <p className="body-medium text-dark500_light700">{title}</p>
+              <Image
+                src={"/assets/icons/chevron-right.svg"}
+                alt="chevron-arrow"
+                height={20}
+                width={20}
+                className="invert-colors"
+              />
+            </Link>
+          ))}
+        </div>
       </div>
       <div className="mt-16">
         <h3 className="h3-bold text-dark200_light900">Popular Tags</h3>
