59e5c761a23bacd47091cbc59e11a568e9818bfd
bsegers <briansegs@gmail.com>
2024-05-22 16:34:12 -0400

    feat: Add Question card

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -3,47 +3,56 @@ import LocalSearchbar from "@/components/shared/search/LocalSearchbar";
 import Filter from "@/components/shared/Filter";
 import HomeFilters from "@/components/home/HomeFilters";
 import NoResult from "@/components/shared/NoResult";
+import QuestionCard from "@/components/cards/QuestionCard";
 import { HomePageFilters } from "@/constants/filters";
 import { Button } from "@/components/ui/button";
 
 const questions = [
   {
-    _id: 1,
+    _id: "1",
     title: "Cascading Deletes in SQLAlchemy?",
     tags: [
       {
-        _id: 1,
+        _id: "1",
         name: "python",
       },
       {
-        _id: 2,
+        _id: "2",
         name: "sql",
       },
     ],
-    author: "John Doe",
-    upvotes: 10,
-    views: 100,
-    answers: 2,
-    createdAt: "2021-09-01T12:00:00.000Z",
+    author: {
+      _id: "1",
+      name: "John Doe",
+      picture: "https://example.com/johndoe.jpg",
+    },
+    upvotes: 5320,
+    views: 183299,
+    answers: [],
+    createdAt: new Date("2023-09-01T12:00:00.000Z"),
   },
   {
-    _id: 2,
+    _id: "2",
     title: "How to center a div?",
     tags: [
       {
-        _id: 1,
+        _id: "3",
         name: "css",
       },
       {
-        _id: 2,
+        _id: "4",
         name: "sql",
       },
     ],
-    author: "John Doe",
-    upvotes: 10,
-    views: 100,
-    answers: 2,
-    createdAt: "2021-09-01T12:00:00.000Z",
+    author: {
+      _id: "2",
+      name: "Jane Smith",
+      picture: "https://example.com/janesmith.jpg",
+    },
+    upvotes: 12891,
+    views: 30203200,
+    answers: [],
+    createdAt: new Date("2021-09-01T12:00:00.000Z"),
   },
 ];
 
@@ -79,7 +88,19 @@ export default function Home() {
 
       <div className="mt-10 flex w-full flex-col gap-6">
         {questions.length > 0 ? (
-          questions.map((question) => "QuestionCard")
+          questions.map((question) => (
+            <QuestionCard
+              key={question._id}
+              _id={question._id}
+              title={question.title}
+              tags={question.tags}
+              author={question.author}
+              upvotes={question.upvotes}
+              views={question.views}
+              answers={question.answers}
+              createdAt={question.createdAt}
+            />
+          ))
         ) : (
           <NoResult
             title="There's no question to show"
