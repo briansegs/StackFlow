d459f610f2625d57eac746b078f11bf8862064b4
bsegers <briansegs@gmail.com>
2024-05-22 16:15:59 -0400

    feat: Create convert Big number function

--- lib/utils.ts
+++ lib/utils.ts
@@ -40,3 +40,13 @@ export const getTimestamp = (createdAt: Date): string => {
 
   return "just now";
 };
+
+export const formatBigNumber = (number: number): string => {
+  if (number >= 1_000_000) {
+    return `${(number / 1_000_000).toFixed(2)}M`;
+  } else if (number >= 1_000) {
+    return `${(number / 1_000).toFixed(2)}K`;
+  } else {
+    return number.toString();
+  }
+};
