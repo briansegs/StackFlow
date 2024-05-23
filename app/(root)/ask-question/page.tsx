4189152eb9ad344a7c10d62c2c1091a3b0b51e4b
bsegers <briansegs@gmail.com>
2024-05-23 15:21:36 -0400

    feat: Setup form and add first field

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,5 +1,15 @@
-const askQuestion = () => {
-  return <div>askQuestion</div>;
+import Question from "@/components/forms/Question";
+
+const Page = () => {
+  return (
+    <div>
+      <h1 className="h1-bold text-dark100_light900">Ask a question</h1>
+
+      <div className="mt-9">
+        <Question />
+      </div>
+    </div>
+  );
 };
 
-export default askQuestion;
+export default Page;
