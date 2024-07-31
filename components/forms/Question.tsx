fb7b34ce6b3f11011d0984cb14f456aac9f43959
bsegers <briansegs@gmail.com>
2024-07-31 10:36:14 -0400

    Create question action

--- components/forms/Question.tsx
+++ components/forms/Question.tsx
@@ -19,6 +19,7 @@ import { QuestionsSchema } from "@/lib/validations";
 import { Badge } from "../ui/badge";
 import Image from "next/image";
 import { createQuestion } from "@/lib/actions/question.action";
+import { useRouter, usePathname } from "next/navigation";
 
 interface Props {
   type?: string;
@@ -29,6 +30,8 @@ interface Props {
 const Question = ({ type, mongoUserId, questionDetails }: Props) => {
   const editorRef = useRef(null);
   const [isSubmitting, setIsSubmitting] = useState(false);
+  const router = useRouter();
+  const pathname = usePathname();
 
   // 1. Define your form.
   const form = useForm<z.infer<typeof QuestionsSchema>>({
@@ -48,9 +51,15 @@ const Question = ({ type, mongoUserId, questionDetails }: Props) => {
       // make an async call to your API -> create a question
       // contain all form data
 
-      await createQuestion({});
+      await createQuestion({
+        title: values.title,
+        content: values.explanation,
+        tags: values.tags,
+        author: JSON.parse(mongoUserId),
+      });
 
       // navigate to home page
+      router.push("/");
     } catch (error) {
     } finally {
       setIsSubmitting(false);
