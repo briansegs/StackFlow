3269a874325f2b8ef9115ca38244d72f7260e528
bsegers <briansegs@gmail.com>
2024-08-02 09:55:27 -0400

    Display questions on the home page

--- components/forms/Question.tsx
+++ components/forms/Question.tsx
@@ -56,6 +56,7 @@ const Question = ({ type, mongoUserId, questionDetails }: Props) => {
         content: values.explanation,
         tags: values.tags,
         author: JSON.parse(mongoUserId),
+        path: pathname,
       });
 
       // navigate to home page
