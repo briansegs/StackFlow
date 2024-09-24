a7b3b009035d26747a93e8f65217dff22cca5577
bsegers <briansegs@gmail.com>
2024-09-24 18:09:04 -0400

    update: env name

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -7,8 +7,8 @@ import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the endpoint
-  // TODO: Add your webhook secret to .env.local
-  const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
+
+  const WEBHOOK_SECRET = process.env.NEXT_CLERK_WEBHOOK_SECRETE;
 
   if (!WEBHOOK_SECRET) {
     throw new Error(
@@ -55,6 +55,8 @@ export async function POST(req: Request) {
   // Do something with the payload
   // For this guide, you simply log the payload to the console
   const eventType = evt.type;
+
+  console.log(eventType);
 
   if (eventType === "user.created") {
     const { id, email_addresses, image_url, username, first_name, last_name } =
