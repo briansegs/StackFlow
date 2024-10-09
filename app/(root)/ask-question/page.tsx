31ade9664cbd30e52712602d990e50cd0f5a4553
bsegers <briansegs@gmail.com>
2024-10-09 17:39:01 -0400

    add: Create tags page

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,11 +1,11 @@
 import Question from "@/components/forms/Question";
 import { getUserById } from "@/lib/actions/user.action";
-// import { auth } from "@clerk/nextjs/server";
+import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 
 const Page = async () => {
-  // const { userId } = auth();
-  const userId = "clerk12345";
+  const { userId } = auth();
+  // const userId = "clerk12345";
 
   if (!userId) {
     redirect("/sign-in");
