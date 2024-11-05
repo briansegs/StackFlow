005da13b2e4f6c117e5ce36b9b168929fbca0837
bsegers <briansegs@gmail.com>
2024-11-05 09:21:12 -0500

    add: Show popular tags

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -3,37 +3,11 @@ import Image from "next/image";
 import RenderTag from "./RenderTag";
 import React from "react";
 import { getHotQuestions } from "@/lib/actions/question.action";
-
-const popularTags = [
-  {
-    _id: "1",
-    name: "Javascript",
-    totalQuestions: 5,
-  },
-  {
-    _id: "2",
-    name: "react",
-    totalQuestions: 12,
-  },
-  {
-    _id: "3",
-    name: "next",
-    totalQuestions: 8,
-  },
-  {
-    _id: "4",
-    name: "vue",
-    totalQuestions: 2,
-  },
-  {
-    _id: "5",
-    name: "redux",
-    totalQuestions: 3,
-  },
-];
+import { getTopPopularTags } from "@/lib/actions/tag.actions";
 
 const RightSidebar = async () => {
   const hotQuestions = await getHotQuestions();
+  const popularTags = await getTopPopularTags();
 
   return (
     <section className="light-border background-light900_dark200 custom-scrollbar sticky right-0 top-0 flex h-screen w-fit flex-col  gap-6 overflow-y-auto border-l p-6 pt-36 shadow-light-300 dark:shadow-none max-xl:hidden lg:w-[350px]">
@@ -61,11 +35,11 @@ const RightSidebar = async () => {
       <div className="mt-16">
         <h3 className="h3-bold text-dark200_light900">Popular Tags</h3>
         <div className="mt-7 flex flex-col gap-4">
-          {popularTags.map(({ _id, name, totalQuestions }) => (
+          {popularTags.map(({ _id, name, numberOfQuestions }) => (
             <RenderTag
               key={_id}
               name={name}
-              totalQuestions={totalQuestions}
+              totalQuestions={numberOfQuestions}
               _id={_id}
               showCount
             />
