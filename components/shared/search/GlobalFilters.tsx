0c40021321e8a12048da692bf0bd6daee698cc2b
bsegers <briansegs@gmail.com>
2024-11-10 12:14:42 -0500

    add: Create global search result component

---
+++ components/shared/search/GlobalFilters.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const GlobalFilters = () => {
+  return (
+    <p className="text-dark400_light900 paragraph-semibold px-5">
+      Global Filters
+    </p>
+  );
+};
+
+export default GlobalFilters;
